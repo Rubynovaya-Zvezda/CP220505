e6c35fbdf871e89881709596d185a2f984b57d13
Vlad Napolov <napolov.v.d@gmail.com>
2022-05-06 19:06:43 +0300

    Player control added
    player is flying like shit it loosy tire, dunno how to fix it. also a bit chenged room 1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 1;
+    Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.Space))
+            rb.AddForce(new Vector2(0, 100f), ForceMode2D.Impulse);
+        if(Input.GetKey(KeyCode.A))
+            rb.AddForce(new Vector2(-30f, 0), ForceMode2D.Impulse);
+        if(Input.GetKey(KeyCode.D))
+            rb.AddForce(new Vector2(30f, 0), ForceMode2D.Impulse);
     }
 }
